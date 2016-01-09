16733278219c4374c5612568c7aacf41fe386d90
WallyWest21 <candymanis@hotmail.com>
2016-01-09 17:30:42 -0500

    Updated UI and 2D Export

---
+++ BOM Export/BOM Export.xaml.vb
@@ -0,0 +1,6 @@
+﻿Public Class BOM_Export
+    Private Sub Label_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
+        MsgBox(oDrawing.PartsList)
+    End Sub
+End Class
---
+++ BOM Export/MainWindow.xaml.vb
@@ -0,0 +1,10 @@
+﻿Imports CATIA_Lib
+
+Class MainWindow
+    Dim CATIA As New Cl_CATIA
+    Dim oProduct As New Cl_CATIA._3D.Product
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        textBlock.Text = oProduct.SelectSingle3DProduct
+        oProduct.test()
+    End Sub
+End Class
