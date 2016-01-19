d0a7e97f3743d474488adacf9c2d04a046c1e0b2
WallyWest21 <candymanis@hotmail.com>
2016-01-19 08:54:03 -0500

    Binded My2DListbox to 2D Table

--- BOM Export/BOM Export.xaml.vb
+++ BOM Export/BOM Export.xaml.vb
@@ -1,6 +1,16 @@
 ﻿Public Class BOM_Export
+    Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
+    Dim oProduct As New CATIA_Lib.Cl_CATIA._3D.oProduct
     Private Sub Label_MouseDown(sender As Object, e As MouseButtonEventArgs)
-        Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
-        MsgBox(oDrawing.PartsList)
+
+        'MsgBox(oDrawing.PartsList.Item(1).PartNo)
+        My2DListBox.ItemsSource = oDrawing.PartsList()
+    End Sub
+
+    Private Sub Label_MouseDown_1(sender As Object, e As MouseButtonEventArgs)
+        'MsgBox(oProduct.SelectSingle3DProduct.product.partnumber)
+        My3DListBox.ItemsSource = oProduct.PartsList()
+        'MsgBox(oProduct.PartsList.Item(0).PartNo)
+        'MsgBox(oProduct.PartsList.Item(0).Nomenclature)
     End Sub
 End Class
--- BOM Export/MainWindow.xaml.vb
+++ BOM Export/MainWindow.xaml.vb
@@ -2,9 +2,9 @@
 
 Class MainWindow
     Dim CATIA As New Cl_CATIA
-    Dim oProduct As New Cl_CATIA._3D.Product
+    Dim oProduct As New Cl_CATIA._3D.oProduct
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        textBlock.Text = oProduct.SelectSingle3DProduct
-        oProduct.test()
+        'textBlock.Text = oProduct.SelectSingle3DProduct
+        'oProduct.test()
     End Sub
 End Class
