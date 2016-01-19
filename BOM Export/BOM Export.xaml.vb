4c4060455d872d38187c304dba688342577893c2
WallyWest21 <candymanis@hotmail.com>
2016-01-19 10:49:15 -0500

    Binded My2DListbox to 2D Table and My3DListbox to 3D Children of product

--- BOM Export/BOM Export.xaml.vb
+++ BOM Export/BOM Export.xaml.vb
@@ -3,11 +3,20 @@
     Dim oProduct As New CATIA_Lib.Cl_CATIA._3D.oProduct
     Private Sub Label_MouseDown(sender As Object, e As MouseButtonEventArgs)
 
+
+
+    End Sub
+
+    Private Sub Label_MouseDown_1(sender As Object, e As MouseButtonEventArgs)
+
+    End Sub
+
+    Private Sub lb_2D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_2D.MouseDown
         'MsgBox(oDrawing.PartsList.Item(1).PartNo)
         My2DListBox.ItemsSource = oDrawing.PartsList()
     End Sub
 
-    Private Sub Label_MouseDown_1(sender As Object, e As MouseButtonEventArgs)
+    Private Sub lb_3D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_3D.MouseDown
         'MsgBox(oProduct.SelectSingle3DProduct.product.partnumber)
         My3DListBox.ItemsSource = oProduct.PartsList()
         'MsgBox(oProduct.PartsList.Item(0).PartNo)
