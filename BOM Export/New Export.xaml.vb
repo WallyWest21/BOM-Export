1554fab6d244adf204543ecdd2ed1a72d5883cb7
WallyWest21 <candymanis@hotmail.com>
2016-01-23 14:06:54 -0500

    Added User Control to Main StackPanel

--- BOM Export/New Export.xaml.vb
+++ BOM Export/New Export.xaml.vb
@@ -1,3 +1,12 @@
-﻿Public Class New_Export
+﻿Imports BOM_Export
+Public Class New_Export
+    Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
+    Dim oProduct As New CATIA_Lib.Cl_CATIA._3D.oProduct
+    Private Sub lb_2D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_2D.MouseDown
+        My2DListBox.ItemsSource = oDrawing.PartsList()
+    End Sub
 
+    Private Sub lb_3D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_3D.MouseDown
+        My3DListBox.ItemsSource = oProduct.PartsList()
+    End Sub
 End Class
---
+++ BOM Export/UIElementz.vb
@@ -0,0 +1,7 @@
+﻿Public Class UIElementz
+    Public Sub AddUserControl(ParentUI As StackPanel, ChildUI As UserControl)
+        'ParentUI.Children.Clear()
+        'ChildUI = New UserControl
+        ParentUI.Children.Add(ChildUI)
+    End Sub
+End Class
