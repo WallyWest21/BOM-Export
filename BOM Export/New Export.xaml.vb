3ba02cef961db99cd53dc7d79be607f5cc2b6dfe
WallyWest21 <candymanis@hotmail.com>
2016-01-26 13:20:20 -0500

    UI Changes

--- BOM Export/New Export.xaml.vb
+++ BOM Export/New Export.xaml.vb
@@ -7,23 +7,29 @@ Public Class New_Export
         lb_2D.Foreground = New SolidColorBrush(Colors.Tan)
 
         My2DListBox.ItemsSource = oDrawing.PartsList()
-        My2DListBox.BorderThickness = New Thickness(1)
+        'My2DListBox.BorderThickness = New Thickness(1)
         'My2DListBox.ItemTemplate = My.Resources.datatemplate("My2DExportTemplate")
         'this.Resources["My2DExportTemplate"] As DataTemplate
-
+        My2DListBox.ItemTemplate = CType(FindResource("My2DDataTemplate"), DataTemplate)
     End Sub
 
     Private Sub lb_3D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_3D.MouseDown
         lb_2D.Foreground = New SolidColorBrush(Colors.White)
         lb_3D.Foreground = New SolidColorBrush(Colors.Tan)
-        My3DListBox.ItemsSource = oProduct.PartsList()
-        My3DListBox.BorderThickness = New Thickness(1)
+        'My3DListBox.ItemsSource = oProduct.PartsList()
+        'My3DListBox.BorderThickness = New Thickness(1)
+
+        'My2DListBox.BorderThickness = New Thickness(1)
+        My2DListBox.ItemsSource = oProduct.PartsList()
+        My2DListBox.ItemTemplate = CType(FindResource("My3DDataTemplate"), DataTemplate)
 
     End Sub
 
     Private Sub tab_SecondTab_Loaded(sender As Object, e As RoutedEventArgs) Handles tab_SecondTab.Loaded
-        tab_SecondTab.Header = "B471356" & vbCrLf & "FACADES"
+        tab_SecondTab.Header = "B471356"
     End Sub
 
+    Sub AddNewExport()
 
+    End Sub
 End Class
