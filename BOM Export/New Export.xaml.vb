487448423c1f5cbccbc7e23e6e1f1db745e56292
WallyWest21 <candymanis@hotmail.com>
2016-01-30 10:04:38 -0500

    Added Error handling if CATIA is not opened

--- BOM Export/New Export.xaml.vb
+++ BOM Export/New Export.xaml.vb
@@ -2,15 +2,19 @@
 Public Class New_Export
     Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
     Dim oProduct As New CATIA_Lib.Cl_CATIA._3D.oProduct
+    Dim oCATIA As New CATIA_Lib.Cl_CATIA
     Private Sub lb_2D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_2D.MouseDown
-        lb_3D.Foreground = New SolidColorBrush(Colors.White)
-        lb_2D.Foreground = New SolidColorBrush(Colors.Tan)
 
-        My2DListBox.ItemsSource = oDrawing.PartsList()
-        'My2DListBox.BorderThickness = New Thickness(1)
-        'My2DListBox.ItemTemplate = My.Resources.datatemplate("My2DExportTemplate")
-        'this.Resources["My2DExportTemplate"] As DataTemplate
-        My2DListBox.ItemTemplate = CType(FindResource("My2DDataTemplate"), DataTemplate)
+        If oCATIA.IsCATIAOpen = True Then
+            lb_3D.Foreground = New SolidColorBrush(Colors.White)
+            lb_2D.Foreground = New SolidColorBrush(Colors.Tan)
+
+            My2DListBox.ItemsSource = oDrawing.PartsList()
+            'My2DListBox.BorderThickness = New Thickness(1)
+            'My2DListBox.ItemTemplate = My.Resources.datatemplate("My2DExportTemplate")
+            'this.Resources["My2DExportTemplate"] As DataTemplate
+            My2DListBox.ItemTemplate = CType(FindResource("My2DDataTemplate"), DataTemplate)
+        End If
     End Sub
 
     Private Sub lb_3D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_3D.MouseDown
@@ -32,4 +36,5 @@ Public Class New_Export
     Sub AddNewExport()
 
     End Sub
+
 End Class
