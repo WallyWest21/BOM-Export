8e6dca27ef64ec396ed7a94f58468ff3a80ee456
WallyWest21 <candymanis@hotmail.com>
2016-02-07 13:24:43 -0500

    Successfully assigned a tab for each 2D sub assembly

--- BOM Export/New Export.xaml.vb
+++ BOM Export/New Export.xaml.vb
@@ -1,14 +1,35 @@
 ﻿
+Imports System.Linq
 Public Class New_Export
     Dim oDrawing As New CATIA_Lib.Cl_CATIA.Drawing
     Dim oProduct As New CATIA_Lib.Cl_CATIA._3D.oProduct
     Dim oCATIA As New CATIA_Lib.Cl_CATIA
 
+    'Dim NewPartslist
     Private Sub lb_2D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_2D.MouseDown
-
+        Dim Parent As String
         If oCATIA.IsCATIAOpen = True Then
-            AddNewExport(sender.Name)
+
+            Dim NewPartslist = From part2D In oDrawing.PartsList()
+                               Select part2D
+
+            Dim ParentPartNumbers = (From ParentPartNumber In NewPartslist
+                                     Select ParentPartNumber.ParentDashNo
+                                     Order By ParentDashNo Ascending).Distinct
+
+
+            For Each ParentPartNo In ParentPartNumbers
+
+
+                Dim New2DPartsList = From part2D In NewPartslist
+                                     Where part2D.ParentDashNo = ParentPartNo
+                                     Select part2D
+
+
+                AddNewExport(sender.Name, New2DPartsList, ParentPartNo)
+            Next
         End If
+
 
 
     End Sub
@@ -16,45 +37,24 @@ Public Class New_Export
     Private Sub lb_3D_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_3D.MouseDown
 
         If oCATIA.IsCATIAOpen = True Then
-            AddNewExport(sender.Name)
+
+            Dim NewPartslist = From part3D In oProduct.PartsList()
+                               Select part3D
+            Dim ParentPartNo As String
+
+            AddNewExport(sender.Name, NewPartslist, ParentPartNo)
         End If
     End Sub
-    Sub AddNewExport(btnName As String)
+    Sub AddNewExport(btnName As String, NewPartslist As Object, ParentPartNo As String)
 
         Dim ExportedItems = New List(Of TabItem)
 
         Dim NewTab As New TabItem()
-        NewTab.Header = "B471356"
+        NewTab.Header = ParentPartNo
         NewTab.Name = "NewName"
 
-        Dim MyExportedListBox As New ListBox
-
-        MyExportedListBox.HorizontalAlignment = HorizontalAlignment.Stretch
-        MyExportedListBox.SelectionMode = SelectionMode.Multiple
-        MyExportedListBox.Background = New SolidColorBrush(Colors.Black)
-        MyExportedListBox.BorderThickness = New Thickness(0)
-        'MyExportedListBox.Height = Double.NaN
-
-        Select Case btnName
-            Case "lb_2D"
-
-                lb_3D.Foreground = New SolidColorBrush(Colors.White)
-                lb_2D.Foreground = New SolidColorBrush(Colors.Tan)
-
-                MyExportedListBox.ItemTemplate = CType(FindResource("My2DDataTemplate"), DataTemplate)
-                MyExportedListBox.ItemsSource = oDrawing.PartsList()
-
-            Case "lb_3D"
-
-                lb_2D.Foreground = New SolidColorBrush(Colors.White)
-                lb_3D.Foreground = New SolidColorBrush(Colors.Tan)
-
-                MyExportedListBox.ItemTemplate = CType(FindResource("My3DDataTemplate"), DataTemplate)
-                MyExportedListBox.ItemsSource = oProduct.PartsList()
-        End Select
-
         'ExportedItems.Insert(0, NewTab)
-        NewTab.Content = MyExportedListBox
+        NewTab.Content = AddListBox(btnName, NewPartslist)
 
         'ExportedItems.Add(NewTab)
         'ExpotedTabsCtrl.DataContext = ExportedItems
@@ -63,8 +63,39 @@ Public Class New_Export
 
 
     End Sub
+    Function AddListBox(btnName As String, NewPartslist As Object) As ListBox
+        'Dim NewPartslist As New List(Of String)
+        Dim MyExportedListBox As New ListBox
 
-    Private Sub lb_excel_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles lb_excel.MouseDown
-        'AddNewExport()
-    End Sub
+        MyExportedListBox.HorizontalAlignment = HorizontalAlignment.Stretch
+        MyExportedListBox.SelectionMode = SelectionMode.Multiple
+        MyExportedListBox.Background = New SolidColorBrush(Colors.Black)
+        MyExportedListBox.BorderThickness = New Thickness(0)
+        'MyExportedListBox.Height = Double.NaN
+
+
+
+        Select Case btnName
+
+            Case "lb_2D"
+
+                lb_3D.Foreground = New SolidColorBrush(Colors.White)
+                lb_2D.Foreground = New SolidColorBrush(Colors.Tan)
+
+                MyExportedListBox.ItemTemplate = CType(FindResource("My2DDataTemplate"), DataTemplate)
+                MyExportedListBox.ItemsSource = NewPartslist
+
+            Case "lb_3D"
+
+                lb_2D.Foreground = New SolidColorBrush(Colors.White)
+                lb_3D.Foreground = New SolidColorBrush(Colors.Tan)
+
+                MyExportedListBox.ItemTemplate = CType(FindResource("My3DDataTemplate"), DataTemplate)
+                MyExportedListBox.ItemsSource = NewPartslist
+
+        End Select
+
+        Return MyExportedListBox
+    End Function
+
 End Class
